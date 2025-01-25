31c51ccce23b6d9488b2b82e2fc3f0b9afb3c40b
lym74 <745371033@qq.com>
2025-01-25 20:13:53 +0800

    feat:init ddd project

---
+++ chatbot-api-interfaces/src/test/java/chatbot/api/test/ApiTest.java
@@ -0,0 +1,10 @@
+package chatbot.api.test;
+
+import chatbot.api.ApiApplication;
+import org.springframework.boot.SpringApplication;
+
+public class ApiTest {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
